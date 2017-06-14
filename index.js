3e8cc1f9a9c8ed50b9aeef58f61cf3b0e8fee621
Max Graey <maxgraey@gmail.com>
2017-06-14 13:25:24 +0300

    Add Allow-Methods

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
