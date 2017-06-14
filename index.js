7b0c1c756f6ad98dbd90ab7bb6063870018ca0e4
Max Graey <maxgraey@gmail.com>
2017-06-14 13:10:19 +0300

    update allowing headers

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
